542c328d1bc60dc6b9e72a207eee34a45dd8bea0
Wayne Dixon <wayne@waynedixon.com>
2019-07-28 16:16:38 -0500

    Added tag 1.0.3

--- Sources/SwiftPackage1/SwiftPackage1.swift
+++ Sources/SwiftPackage1/SwiftPackage1.swift
@@ -1,5 +1,5 @@
 struct SwiftPackage1 {
-    var text = "Swift Package with tag of 1.0.2!!"
+    var text = "Swift Package with tag of 1.0.3!!"
     open public func getText() -> String {
         return text
     }
