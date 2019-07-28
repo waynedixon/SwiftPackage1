f8b78f7e18df9315f8050a11fa1fc96c60340c44
Wayne Dixon <wayne@waynedixon.com>
2019-07-28 16:45:26 -0500

    1.0.0

--- Sources/SwiftPackage1/SwiftPackage1.swift
+++ Sources/SwiftPackage1/SwiftPackage1.swift
@@ -1,3 +1,4 @@
 public struct SwiftPackage1 {
-    public var text = "SwiftPackage1 - 1.0.6!!"
+    public var text = "SwiftPackage1 - 1.0.0!!"
+    public func getText() -> String { return text }
 }
