4664e3e6c75a8702041b92d6f1faf242d4bf903f
Wayne Dixon <wayne@waynedixon.com>
2019-07-28 16:08:06 -0500

    Modified text and function.

--- Sources/SwiftPackage1/SwiftPackage1.swift
+++ Sources/SwiftPackage1/SwiftPackage1.swift
@@ -1,5 +1,5 @@
 struct SwiftPackage1 {
-    var text = "Hello, World, I am a Swift Package!"
+    var text = "Hello, World, I am a Swift Package with a tag of 1.0.2"
     public func getText() -> String {
         return text
     }
