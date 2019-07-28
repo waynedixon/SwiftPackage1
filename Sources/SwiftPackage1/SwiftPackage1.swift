990130d36e104ea6b2da4dfc62ceaf236c50d5b5
Wayne Dixon <wayne@waynedixon.com>
2019-07-28 15:53:37 -0500

    A second commit.

--- Sources/SwiftPackage1/SwiftPackage1.swift
+++ Sources/SwiftPackage1/SwiftPackage1.swift
@@ -1,5 +1,5 @@
 struct SwiftPackage1 {
-    var text = "Hello, World, I am a Swift Package with tag of 1.0.0!"
+    var text = "Hello, World, I am a Swift Package with tag of 1.0.0!!"
     func getText() -> String {
         return text
     }
